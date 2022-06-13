5b82ffaa5b29685fbdc2904345d0eb157abbe212
Rene Poncelet <re.poncelet@gmail.com>
2022-06-13 10:27:07 +0100

    updated install script

--- HEPSoftware/install-all.sh
+++ HEPSoftware/install-all.sh
@@ -1,43 +1,49 @@
 #!/bin/bash
 
+dir=$(pwd)
+
+
+echo '#############################'
+echo 'Install HEPSoftware to : '$dir
+echo '#############################'
+echo ''
+
 # Dependencies:
- sudo apt install gcc g++ python-dev zlib1g-dev gfortran git autoconf automake texlive texlive-pstricks imagemagick-6.q16hdri libtool
+sudo apt install gcc g++ python-dev zlib1g-dev gfortran git autoconf automake texlive texlive-pstricks imagemagick-6.q16hdri libtool
 
-
-#### first run the rivet bootstrap script which installs most of the software
+##### first run the rivet bootstrap script which installs most of the software
 cd build
 mkdir rivet
 cd rivet
 wget https://gitlab.com/hepcedar/rivetbootstrap/raw/3.1.0/rivet-bootstrap
 chmod +x rivet-bootstrap
-INSTALL_PREFIX=/home/tutorial/HEPSoftware/install ./rivet-bootstrap
+INSTALL_PREFIX=$dir/install ./rivet-bootstrap
 
-#### install LHAPDF
-cd /home/tutorial/HEPSoftware/tarballs
+##### install LHAPDF
+cd $dir/tarballs
 wget https://lhapdf.hepforge.org/downloads/LHAPDF-6.2.3.tar.gz
-cd /home/tutorial/HEPSoftware/build
+cd $dir/build
 tar -xf ../tarballs/LHAPDF-6.2.3.tar.gz
 cd LHAPDF-6.2.3
-./configure --prefix=/home/tutorial/HEPSoftware/install
+./configure --prefix=$dir/install
 make
 make install
-#### install NNPDF 31 as an example
-wget http://lhapdfsets.web.cern.ch/lhapdfsets/current/NNPDF31_nnlo_as_0118.tar.gz -O- | tar xz -C /home/tutorial/HEPSoftware/install/share/LHAPDF
+##### install NNPDF 31 as an example
+wget http://lhapdfsets.web.cern.ch/lhapdfsets/current/NNPDF31_nnlo_as_0118.tar.gz -O- | tar xz -C $dir/install/share/LHAPDF
 
-#### install OpenLoops
-cd /home/tutorial/HEPSoftware/build
+##### install OpenLoops
+cd $dir/build
 git clone https://gitlab.com/openloops/OpenLoops.git
 cd OpenLoops
 ./scons
 ./openloops libinstall pptt
-# make lib public
-ln -s ./lib/* /home/tutorial/HEPSoftware/install/lib/
-ln -s ./include/* /home/tutorial/HEPSoftware/install/include/
+ln -s ./lib/* $dir/install/lib/
+ln -s ./include/* $dir/install/include/
 
-#### install Sherpa
-cd /home/tutorial/HEPSoftware/tarballs
+##### install Sherpa
+cd $dir/tarballs
 wget https://sherpa.hepforge.org/downloads/SHERPA-MC-2.2.8.tar.gz
-cd /home/tutorial/HEPSoftware/build
+cd $dir/build
 tar -xf ../tarballs/SHERPA-MC-2.2.8.tar.gz
 cd SHERPA-MC-2.2.8
 ./configure 
