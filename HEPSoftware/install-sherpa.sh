7237f03b31cb6e18a4283c81d720bcd014dd2e10
Rene Poncelet <dummy@gmail.com>
2022-06-13 10:48:24 +0100

    updated sherpa installation script

--- HEPSoftware/install-sherpa.sh
+++ HEPSoftware/install-sherpa.sh
@@ -14,4 +14,6 @@ wget https://sherpa.hepforge.org/downloads/SHERPA-MC-2.2.8.tar.gz
 cd $dir/build
 tar -xf ../tarballs/SHERPA-MC-2.2.8.tar.gz
 cd SHERPA-MC-2.2.8
-./configure 
+./configure --prefix=$dir/install --with-sqlite3=install --enable-hepmc2=$dir/install --enable-lhapdf=$dir/install --enable-rivet=$dir/install --enable-openloops=$dir/install
+make
+make install
